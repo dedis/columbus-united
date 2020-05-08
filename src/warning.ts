74657577c2fcd44f8a2e445ad8427d9ec01ec1d1
Julien <julien.vonfelten@epfl.ch>
2020-05-08 18:25:52 +0200

    Add container of warnings and implement all warnings with click button

--- src/warning.ts
+++ src/warning.ts
@@ -1,23 +1,40 @@
+import * as d3 from "d3";
 
+export class Warning {
+    containerWarnings: d3.Selection<HTMLDivElement, unknown, HTMLElement, any>;
 
-export class Warning{
-    constructor(){ 
+  constructor() {
+      this.containerWarnings = d3.select("body").append("div").attr("class", "alertContainer")
+  }
+
+  public displaying(typeError: number, text: string) {
+    //display the block and the error with colors
+    let span = undefined;
+    if (typeError === 1) {
+      let divAlert = this.containerWarnings.append("div").attr("class", "alert");
+      span = divAlert.append("span").attr("class", "closebtn").text(`x`);
+      divAlert.append("text").text(`Error: ${text}`);
+    } else if (typeError === 2) {
+      let divAlert = this.containerWarnings.append("div").attr("class", "alert warning");
+      span = divAlert.append("span").attr("class", "closebtn").text(`x`);
+      divAlert.append("text").text(`Warning: ${text}`);
+    } else if (typeError === 3) {
+      let divAlert = this.containerWarnings.append("div").attr("class", "alert info");
+      span = divAlert.append("span").attr("class", "closebtn").text(`x`);
+      divAlert.append("text").text(`Info: ${text}`);
+    } else {
+      let divAlert = this.containerWarnings.append("div").attr("class", "alert other");
+      span = divAlert.append("span").attr("class", "closebtn").text(`x`);
+      divAlert.append("text").text(`Other error not handled: ${text}`);
     }
 
-    public displaying(typeError:number, text:string){
-        //display the block and the error with colors
-        if(typeError === 1){
-            console.log("Display1: "+typeError+" "+text)
-
-        }else if(typeError === 2){
-            console.log("Display2: "+typeError+" "+text)
-
-        }else if(typeError === 3){
-            console.log("Display3: "+typeError+" "+text)
-
-        }else{
-            console.log("Display4: "+typeError+" "+text)
-
-        }
-    }
+      span.on("click", function () {
+        let div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function () {
+          div.style.display = "none";
+        }, 200);
+      });
+    
+  }
 }
