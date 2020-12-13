7442230abed31b472516a6f85a93e9d7819a1da9
Sophia Artioli <sophia.artioli@epfl.ch>
2020-12-13 23:24:29 +0100

    Last working version of translation

--- src/search.ts
+++ src/search.ts
@@ -13,39 +13,41 @@ import {
     PaginateRequest,
     PaginateResponse,
 } from "@dedis/cothority/byzcoin/proto/stream";
-import { zoom } from 'd3';
-import { select } from 'd3';
-import { ChainConfig } from '@dedis/cothority/byzcoin';
-import { flatMap, switchMap, takeUntil } from 'rxjs/operators';
+import { zoom } from "d3";
+import { select } from "d3";
+import { ChainConfig } from "@dedis/cothority/byzcoin";
+import { flatMap, last, switchMap, takeUntil } from "rxjs/operators";
 
+let arrayBlock = new Array<SkipBlock>();
 
+let activate =false;
 
+let subject = new Subject();
 
-let arrayBlock= new Array();
-
-let activate:boolean;
-
-let subject= new Subject();
-
-export function getActivate(){
+export function getActivate():boolean {
     return this.activate;
 }
-export function getSubj(){
+export function getSubj() {
     return this.activate;
 }
-
+export function setActivate() {
+    this.activate = false;
+}
+export function getBlock() {
+    return this.arrayBlock[0];
+}
 export function searchBar(
     roster: Roster,
     flash: Flash,
     blockSubject: Subject<SkipBlock>,
     hashBlock0: string,
-    chain:Chain
+    chain: Chain,
+    initialBlock:SkipBlock
 ) {
-
     d3.select("#search-input").on("keypress", function () {
         if (d3.event.keyCode === 13) {
             var input = d3.select("#search-input").property("value");
-            const search_mode = d3.select("#search-mode").property("value")
+            const search_mode = d3.select("#search-mode").property("value");
             searchRequest(
                 input,
                 roster,
@@ -53,16 +55,25 @@ export function searchBar(
                 blockSubject,
                 hashBlock0,
                 chain,
-                search_mode
+                search_mode,
+                initialBlock
             );
-            
-        }
+        } 
     });
 
     d3.select("#submit-button").on("click", async function () {
         var input = d3.select("#search-input").property("value");
         const search_mode = d3.select("#search-mode").property("value");
-        searchRequest(input, roster, flash, blockSubject, hashBlock0, chain, search_mode);
+        searchRequest(
+            input,
+            roster,
+            flash,
+            blockSubject,
+            hashBlock0,
+            chain,
+            search_mode,
+            initialBlock
+        );
     });
 }
 
@@ -73,11 +84,11 @@ async function searchRequest(
     blockSubject: Subject<SkipBlock>,
     hashBlock0: string,
     chain: Chain,
-    search_mode : string
+    search_mode: string,
+    initialBlock:SkipBlock
 ) {
     if (search_mode == "hash") {
         try {
-   
             let hi = await Utils.getBlock(Buffer.from(input, "hex"), roster);
 
             flash.display(
@@ -85,101 +96,79 @@ async function searchRequest(
                 "Valid search for block index: " + hi.index.toString()
             );
 
-            // d3.select("#svg-container").selectAll('*').remove();
-            
-            // let newc = new Chain(roster,flash,hi);
-            // chain = newc;
-
-           
-
-            //     d3.selectAll("gblocks").remove();
-            //    let newChain = new Chain(roster,flash,hi);
-            //    chain =newChain;
-            // chain = newChain; 
-            //  blockSubject.next(hi);
-            //  chain.subjectBrowse.next([chain.nbPages,ch,false])
-            //  startColumbus(hi,roster,flash,i);
-          // let plouf = { x: -10, y: 0, k: 1 };
-      
-            
-            //  chain.getNextBlocks(Utils.bytes2String(hi.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
-
-        } catch (error) {
         
+        
+
+            //  chain.getNextBlocks(Utils.bytes2String(hi.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
+        } catch (error) {
             flash.display(Flash.flashType.ERROR, "Block does not exist");
         }
-    }else if(search_mode=="id"){
-
-
+    } else if (search_mode == "id") {
     } else {
-        //try {
-            let block = await Utils.getBlockByIndex(
-                Utils.hex2Bytes(hashBlock0),
-                parseInt(input, 10), 
-                roster
-            );
+        try {
+        let block = await Utils.getBlockByIndex(
+            Utils.hex2Bytes(hashBlock0),
+            parseInt(input, 10)-1, //using subjectBrowse shows one block too far
+            roster
+        );
+        let blockByIndex = block.index;
+        arrayBlock.push(block);
 
+        //    chain.initialBlockIndex= block.index;
+        //     chain.getNextBlocks(Utils.bytes2String(block.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
+        //let sub = new Subject<[number, SkipBlock[], boolean]>();
 
-
-           //    chain.initialBlockIndex= block.index;
-      //     chain.getNextBlocks(Utils.bytes2String(block.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
-           //let sub = new Subject<[number, SkipBlock[], boolean]>();
-           
-            // let obs = sub.asObservable();
-
-
-          // chain.subjectBrowse.pipe(takeUntil());
-           
-        //   chain.subjectBrowse.pipe(switchMap(async function () { chain.subjectBrowse.next([chain.nbPages,arrayBlock,false])} )).subscribe();
-    
-          // sub.next([chain.nbPages,arrayBlock,false]);
-            activate=true;
-            arrayBlock.push(block);
-            
-            let blockByIndex = block.index;
-            let newZoom= d3.zoomIdentity.translate((108900-blockByIndex+6)*110, 0).scale(1);
-          
-          d3.select("#svg-container").call(chain.zoom.transform,newZoom);
-        
-          
-          let lastBlock = await Utils.getBlockByIndex(Utils.hex2Bytes(hashBlock0),108049,roster);
-          chain.lastBlockRight=lastBlock;
-        
-        //   chain.initialBlock= block;
-        //   chain.initialBlockIndex=block.index;
-        //   //arrayBlock.push(chain.lastBlock);
-        //   chain.initialBlock= block;
-        //   chain.initialBlockIndex=block.index;
-        //   chain.lastBlock=block;
-        //   chain.lastBlockRight=lastBlock;
-        //   console.log("<<<<<<<<<<<<<<<<"+chain.lastBlockRight.index);
-
-   
-        
-        chain.getNextBlocks(Utils.bytes2String(block.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
-        chain.subjectBrowse.next([chain.nbPages,arrayBlock,false])
        
-            
+
+        // sub.next([chain.nbPages,arrayBlock,false]);
+        //activate = true;
+  //      chain.subjectBrowse.complete();
+
+    
+  chain.subjectBrowse.next([chain.nbPages,arrayBlock,false]);
+
+
+// doesn't display the blocks
+//chain.getNextBlocks(Utils.bytes2String(block.hash),chain.pageSize,chain.nbPages,chain.subjectBrowse,false);
+
+ //when zooming or scaling, the view comesback to the original block
+// chain.subject.next(newZoom);
+        
+  //translate the chain to wanted coordinates
+        let newZoom = d3.zoomIdentity
+            .translate((initialBlock.index - blockByIndex-1) * 110 +0.2, 0)  //block+padding =110
+            .scale(1);
+            d3.select("#svg-container").call(chain.zoom.transform, newZoom);
+    
+
+        // let lastBlock = await Utils.getBlockByIndex(
+        //     Utils.hex2Bytes(hashBlock0),
+        //     108049,
+        //     roster
+        // );
+
+        //   chain.lastBlockRight=lastBlock;
+        //   chain.lastBlockLeft=block;
         //   chain.initialBlock= block;
         //   chain.initialBlockIndex=block.index;
-        //   chain.lastBlock=block;
-        //   chain.lastBlockRight=lastBlock;
-        //   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>"+chain.lastBlockRight.index);
+        //  //arrayBlock.push(chain.lastBlock);
+        // //   chain.lastBlock=block;
 
-   
-
-
-
+//Trying to replace old Subject with new one, not efficient 
+        // chain.subjectBrowse.complete();
+        // chain.searchSubject.next([chain.nbPages,arrayBlock,false]);
         
-            flash.display(
-                Flash.flashType.INFO,
-                "Valid search for block index: " + blockByIndex.toString()
-            );
-      
 
-        // } catch (error) {
-        //     // try transactions
-        //     flash.display(Flash.flashType.ERROR, "Block does not exist");
-        // }
+    chain.blockClickedSubject.next(await Utils.getBlockByIndex(Utils.hex2Bytes(hashBlock0),blockByIndex+1,roster));
+        
+        flash.display(
+            Flash.flashType.INFO,
+            "Valid search for block index: " + (blockByIndex+1).toString()
+        );
+
+        } catch (error) {
+           
+            flash.display(Flash.flashType.ERROR, "Block does not exist");
+        }
     }
 }
